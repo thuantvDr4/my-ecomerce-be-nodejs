2aa9dfceef5482877a705128e76b3c6e54de60c8
thuanCafe <vinhthuan.tr@gmail.com>
2025-11-07 20:51:03 +0700

    #2 connect mongodb

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.use(morgan("dev")); // combined -> for PROD | dev | short | tiny | common
 app.use(helmet());
 app.use(compression());
 //init db
+require("./dbs/init.mongodb.lv0");
 
 //init routes
 app.get("/", (req, res, next) => {
