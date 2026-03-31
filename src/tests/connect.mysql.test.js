44f578a537e99d0401b9e4b8ebdcb389b4de7bee
thuanCafe <vinhthuan.tr@gmail.com>
2026-03-31 23:10:38 +0700

    insert 10M record by nodejs

---
+++ src/tests/connect.mysql.test.js
@@ -0,0 +1,52 @@
+// @ts-nocheck
+"use strict";
+const { times } = require("lodash");
+const mysql = require("mysql2");
+
+//--create connection to pool server
+const pool = mysql.createPool({
+  host: "localhost",
+  port: 8811,
+  user: "root",
+  password: "root123456",
+  database: "test",
+});
+
+const batchSize = 10; //1000; // adjust batch size as needed | là kích thước từng lô muốn insert
+const totalSize = 1000; //1_000_000; // adjust total size as needed
+let currentId = 1;
+console.time("::::[TIMER]::::::");
+const insertBatch = async () => {
+  const values = [];
+  // -----loop through batch size
+  for (let i = 0; i < batchSize && currentId <= totalSize; i++) {
+    const name = `Name ${currentId}`;
+    const email = `email${currentId}@gmail.com`;
+    const age = currentId;
+    values.push([currentId, name, age, email]);
+    currentId++;
+  }
+  //--check
+  if (!values.length) {
+    console.timeEnd("::::[TIMER]::::::");
+    pool.end((err) => {
+      if (err) {
+        console.error(`Error occurred while running batch: ${err}`);
+      } else {
+        console.log("connection Pool closed successfully");
+      }
+    });
+    return;
+  }
+  //---sql insert to table
+  const sql = `INSERT INTO test_users (id, name, age, email) VALUES ?`;
+  pool.query(sql, [values], async (err, results) => {
+    if (err) throw err;
+    // inserting
+    console.log(`Inserted ${results?.affectedRows} rows`);
+    await insertBatch();
+  });
+  //
+};
+
+insertBatch().catch((err) => console.error(err));
